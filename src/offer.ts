25801e369e851fba863cb4b7b4a64c8ef5473907
JeffGreat <jf.gruet@gmail.com>
2020-08-25 15:41:37 +0200

    add enable offer

--- src/offer.ts
+++ src/offer.ts
@@ -2,6 +2,7 @@ import { IBaseModel } from './base-model';
 
 export interface IOffer extends IBaseModel {
     teaser: string;
+    enabled: boolean;
     startTime: number;
     endTime: number;
     days: number[];
